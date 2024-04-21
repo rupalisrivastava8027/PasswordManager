1ef5dc4af600b458ef62aa9483b53d471a11b442
Aarush <rupalisrivastava8027@gmail.com>
2024-04-20 20:51:12 -0500

    h

--- main/script.js
+++ main/script.js
@@ -1,82 +1,70 @@
-let all = [];
+let credentials = new Map();
+
+let realPassword = "";
 
 function add()
 {
-
+    let domain = prompt("Domain: ");
     let username = prompt("Username: ");
     let password = prompt("Password: ");
-    let displayPassword = "";
 
-    for (let i = 0; i < password.length; i++)
-    {
-        displayPassword += "#";
-    }
-
-    displayPassword += "!";
-
-
-    let a = [username, password, displayPassword, localStorage.getItem("Credentials")];
-
-    localStorage.setItem("Credentials", a);
-    //console.log(localStorage.getItem(""));
+    localStorage.setItem(domain.toLowerCase(), [username, password]);
 }
 
-function process()
-{
-    let all = [];
-    let temp = []; 
-
-    let string = "";
-
-    let credentials = localStorage.getItem("Credentials");
-
-    for (let i = 0; i < credentials.length; i++)
-    {
-        //console.log(credentials[i]);    
-
-        if (credentials[i] == ",")
-        {
-            temp.push(string);
-            string = "";
-            continue;
-        }
-
-        if (credentials[i] == "!")
-        {
-            temp.push(string);
-            string = "";
-            all.push(temp);
-            temp = [];
-            continue;  
-        }
-        string += credentials[i];
-        
-        
-    }
-    //onsole.log(all.toString());
-    return all;
-    
-    // [ [Domain, username, password, displayPassword], ]
-}
 function getCredentials()
 {
     let find = document.getElementById("find").value;
-
-    document.getElementById("paragraph").innerHTML = "Searching for " + find + " ...";
-
-    let credentials = process();
-
-    console.log(credentials.toString());
-
-    for (let i = 0; i < credentials.length; i++)
+    
+    document.getElementById("search").innerHTML = "Searching for " + find + " ...";
+    
+    if (localStorage.getItem(find.toLowerCase()) != null)
     {
-        if (credentials[i][0].toLowerCase() == find.toLowerCase())
-        {
-            console.log("Found password: " + credentials[i][1]);
-            document.getElementById("result").innerHTML = "Found password: " + credentials[i][1];
-        }
+        document.getElementById("result").innerHTML= "Found " + find + ".";
     }
+
+    else
+    {
+        document.getElementById("result").innerHTML= "Not Found " + find + ".";
+    }
+    const foundCredentials = localStorage.getItem(find.toLowerCase()).toString().split(",");
+    realPassword = foundCredentials[1];
+
+
+    document.getElementById("username").innerHTML = "Username: " + foundCredentials[0];
+
+    document.getElementById("password").innerHTML = "Password: " + toHidden(realPassword); 
+    // console.log(credentials.toString());
+
+    // for (let i = 0; i < credentials.length; i++)
+    // {
+    //     if (credentials[i][0].toLowerCase() == find.toLowerCase())
+    //     {
+    //         console.log("Found password: " + credentials[i][1]);
+    //         document.getElementById("result").innerHTML = "Found password: " + credentials[i][1];
+    //     }
+    // }
 
     
 }
 
+function toHidden(password)
+{
+    let hidden = "";
+
+    for (let i = 0; i < password.length; i++)
+    {
+        hidden += "*";
+    }
+    return hidden;
+}
+
+function changeFromHidden()
+{
+    document.getElementById("password").innerHTML = "Password: " + realPassword;
+}
+
+function changeToHidden()
+{
+    document.getElementById("password").innerHTML = "Password: " + toHidden(realPassword);
+
+}
